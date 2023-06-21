8bb26f4af96e4bcbdcf7c71397740cae18c48bb9
cudymvl <cudy.kim@mvlchain.io>
2023-06-21 20:58:16 +0900

    feat: 데이터 연동하기

--- lib/presentation/scan_page/input_view.dart
+++ lib/presentation/scan_page/input_view.dart
@@ -61,40 +61,9 @@ class _InputViewState extends State<InputView> {
 
   @override
   void initState() {
-    // processImage(widget.image);
+    processImage(widget.image);
     super.initState();
-    // loadModel().then((value) {
-    //   setState(() {});
-    // });
   }
-
-  // // 모델과 label.txt를 가져온다.
-  // loadModel() async {
-  //   await Tflite.loadModel(
-  //     // 모델 넣기
-  //     model: "assets/model_unquant.tflite",
-  //     // 이건 뭔지 모르겠네
-  //     labels: "assets/testlabel.txt",
-  //   ).then((value) {
-  //     setState(() {
-  //       //_loading = false;
-  //     });
-  //   });
-  // }
-
-  // 이미지 분류
-  // Future classifyImage(File image) async {
-  //   // 아래 잘 넣어보기
-  //   var output = await Tflite.runModelOnImage(
-  //       path: image.path,
-  //       imageMean: 0.0, // defaults to 117.0
-  //       imageStd: 255.0, // defaults to 1.0
-  //       numResults: 2, // defaults to 5
-  //       threshold: 0.2, // defaults to 0.1
-  //       asynch: true // defaults to true
-  //       );
-  //   setState(() {});
-  // }
 
   late final TextEditingController itemName = TextEditingController(text: "");
 
